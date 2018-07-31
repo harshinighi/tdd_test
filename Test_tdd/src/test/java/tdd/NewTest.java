892c2e4efa57de6405785dda741bf8448df5a183
IBM <IBM@DEVOPS_10>
2018-07-31 11:29:33 +0530

    jvugovu

--- Test_tdd/src/test/java/tdd/NewTest.java
+++ Test_tdd/src/test/java/tdd/NewTest.java
@@ -23,7 +23,7 @@ public class NewTest {
 	  System.setProperty("webdriver.gecko.driver","E:\\gecko\\geckodriver.exe");
 		driver=new FirefoxDriver();
 		
-		driver.get("http://ec2-18-217-71-1.us-east-2.compute.amazonaws.com:8090/TestWebapp/");
+		driver.get("http://ec2-18-220-4-236.us-east-2.compute.amazonaws.com:8090/TestWebapp/");
 		driver.manage().window().maximize();
 		
   }
