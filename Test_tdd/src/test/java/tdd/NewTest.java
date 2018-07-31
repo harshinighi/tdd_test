25f72ffafd986726a9baf836ef46832db697eea8
IBM <IBM@DEVOPS_10>
2018-07-31 11:15:37 +0530

    jbhubhb

---
+++ Test_tdd/src/test/java/tdd/NewTest.java
@@ -0,0 +1,36 @@
+package tdd;
+
+import org.testng.annotations.Test;
+import org.testng.annotations.BeforeTest;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.AfterTest;
+
+public class NewTest {
+	WebDriver driver;
+  @Test
+  public void f() throws InterruptedException {
+	  driver.findElement(By.xpath("//*[@name='uname']")).sendKeys("admin");
+	  driver.findElement(By.xpath("//*[@name='psw']")).sendKeys("pass");
+	  
+	  driver.findElement(By.xpath("//*[@type='submit']")).click();
+	  Thread.sleep(10000);
+	  
+  }
+  @BeforeTest
+  public void beforeTest() {
+	  System.setProperty("webdriver.gecko.driver","E:\\gecko\\geckodriver.exe");
+		driver=new FirefoxDriver();
+		
+		driver.get("http://ec2-18-217-71-1.us-east-2.compute.amazonaws.com:8090/TestWebapp/");
+		driver.manage().window().maximize();
+		
+  }
+
+  @AfterTest
+  public void afterTest() {
+	  driver.close();
+  }
+
+}
